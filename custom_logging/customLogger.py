961e370c264b5ed66cac2fc52dbefff6f64a43c7
Saif Ali <saifali199511@gmail.com>
2023-03-14 03:32:22 +0530

    added working logging feature into s3 aws

--- custom_logging/customLogger.py
+++ custom_logging/customLogger.py
@@ -1,45 +1,61 @@
 import inspect
 import logging
 import boto3
-from logging.handlers import RotatingFileHandler
+#from logging.handlers import RotatingFileHandler
 
 class S3Handler(logging.Handler):
-    def __init__(self, bucket_name, folder_path):
+
+    def __init__(self, bucket_name, folder_path, file_name):
         super().__init__()
         self.bucket_name = bucket_name
         self.folder_path = folder_path
-        self.s3 = boto3.client('s3')
+        self.log_file_name = file_name
+        self.s3 = boto3.client(service_name='s3',
+                    region_name='eu-north-1')
+                    #aws_access_key_id='value',
+                    #aws_secret_access_key='value'
+                    
 
     def emit(self, record):
+        existing_data = ''
+        key_name = self.folder_path + '/' + self.log_file_name + '.log'
         try:
-            key_name = self.folder_path + '/' + self.format(record) + '.log'
-            self.s3.put_object(Body=self.format(record), Bucket=self.bucket_name, Key=key_name)
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=key_name)
+            existing_data = response['Body'].read().decode('utf-8')
+        except self.s3.exceptions.NoSuchKey:
+            pass
         except Exception as e:
-            print('log file save failed')
+            print(f'error in file read with :: {e}')
+        try:
+            new_data = self.format(record)
+            data = existing_data + "\n" + new_data
+        except Exception as e:
+            print(f'error while appending in text variable with :: {e}')
+        
+        try:
+            
+            self.s3.put_object(Body=data, Bucket=self.bucket_name, Key=key_name)
+            #self.s3.Object(self.bucket_name, key_name).put(Body=self.format(record))
+        except Exception as e:
+            print(f'log file save failed with error :: {e}')
+
+
 
 class custLogger:
 
     def __init__(self,logLevel=logging.INFO):
         self.logLevel = logLevel
-        self.s3_handler=S3Handler(bucket_name='saifineuronproject',folder_path='log_files')
+        self.s3_handler=S3Handler(bucket_name='saifineuronproject',folder_path='log_files',file_name="ineuron_scrapper")
 
     def custlogger(self,logger_name=False):
-        
-        #logger_name = inspect.stack()[1][3]
-        #logger = logging.getLogger(logger_name)
-        #inspect.stack()[1][1].split("/")[-1].split(".")[0]
-        
-        if logger_name:
-            pass
-        else:
-            logger_name = f'{inspect.stack()[1][1].split(f"{chr(92)}")[-1]} | {inspect.stack()[1][3]} | lineno-{inspect.stack()[1][2]}'
+        logger_name = f'{inspect.stack()[1][1].split(f"{chr(92)}")[-1]} | {inspect.stack()[1][3]} | lineno-{inspect.stack()[1][2]}'
         
         logger = logging.getLogger(str(logger_name))
 
         logger.setLevel(self.logLevel)
-        #fh=RotatingFileHandler("C:\\Users\\saifa\\OneDrive\\Desktop\\ineuron projects\\my_own_ineuron_proj\\custom_logging\\ineuron_scrapper.log",maxBytes=10000,backupCount=5)
-        #fh=logging.FileHandler("C:\\Users\\saifa\\OneDrive\\Desktop\\ineuron projects\\my_own_ineuron_proj\\custom_logging\\ineuron_scrapper.log")
+        
         formatter= logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.s3_handler.setFormatter(formatter)
         logger.addHandler(self.s3_handler)
+        
         return logger
